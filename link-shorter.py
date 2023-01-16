8fc14e90c1151e6fe7b6946cef717db4d200a4f1
Fedor <122624582+FedorsHackStation@users.noreply.github.com>
2023-01-16 14:03:38 +0100

    Add files via upload

---
+++ link-shorter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pyshorteners
+import platform
+import os
+
+opsys = platform.platform()
+cls = "clear"
+if ("Linux" in opsys) or ("linux" in opsys) or ("Mac" in opsys) or ("mac" in opsys):
+    cls = "clear"
+elif ("Windows" in opsys) or ("windows" in opsys):
+    cls = "cls"
+os.system(cls)
+
+while True:
+	try:
+		print("Press enter to quit!")
+		link = input("Enter link: ")
+		os.system(cls)
+		if len(link) == 0:
+			break
+		shortener = pyshorteners.Shortener()
+		x = shortener.tinyurl.short(link)
+		print(f"your link: {x}")
+	except:
+		pass
